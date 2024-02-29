150aeb5a0da69b660673a418197724ed35c2eb54
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-02-29 14:31:31 +0100

    works prior to upscaling to 1920

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,67 @@ use gif::{Encoder, Frame, Repeat};
 use std::borrow::Cow;
 use std::fs::File;
 
-const WIDTH: usize = 6;
-const HEIGHT: usize = 6;
+const WIDTH: usize = 386;
+const HEIGHT: usize = 218;
+
+const MARGIN: usize = 10;
 
 fn main() {
-    let iters = 2;
+    let iters = 10000;
     let mut state = Life {
-        state: [
-            [false, false, false, false, false, false],
-            [false, true, true, false, false, false],
-            [false, true, false, false, false, false],
-            [false, false, false, false, true, false],
-            [false, false, false, true, true, false],
-            [false, false, false, false, false, false],
-        ],
+        state: [[false; WIDTH]; HEIGHT],
     };
+    let init = (300, 120);
+    state.state[init.1][init.0 + 1] = true;
+    state.state[init.1 + 1][init.0 + 3] = true;
+    state.state[init.1 + 2][init.0] = true;
+    state.state[init.1 + 2][init.0 + 1] = true;
+    state.state[init.1 + 2][init.0 + 4] = true;
+    state.state[init.1 + 2][init.0 + 5] = true;
+    state.state[init.1 + 2][init.0 + 6] = true;
 
-    let mut ret: Vec<[[bool; WIDTH]; HEIGHT]> = Vec::new();
+    let color_map = &[0x29, 0x2e, 0x42, 0xc4, 0x7f, 0xd5];
+    let mut image = File::create("tests/samples/beacon.gif").unwrap();
+    let mut encoder = Encoder::new(
+        &mut image,
+        (WIDTH - MARGIN) as u16,
+        (HEIGHT - MARGIN) as u16,
+        color_map,
+    )
+    .unwrap();
+    encoder.set_repeat(Repeat::Infinite).unwrap();
     for _ in 0..iters {
-        println!("{state:?}");
-        ret.push(state.state);
-        state = state.update();
-    }
-    let states = ret.iter().map(|x| {
-        x.iter()
+        let x = state.state;
+        let curr = x.iter()
+            .skip(1)
+            .take(HEIGHT - MARGIN)
             .map(|x| {
                 x.iter()
+                    .skip(1)
+                    .take(WIDTH - MARGIN)
                     .map(|x| if *x { 1 } else { 0 })
                     .collect::<Vec<u8>>()
             })
             .enumerate()
-            .fold([0; WIDTH * HEIGHT], |mut sum, curr| {
-                for i in 0..WIDTH {
-                    sum[i + curr.0 * WIDTH] = curr.1[i];
-                }
-                sum
-            })
-    });
-    let color_map = &[0xFF, 0xFF, 0xFF, 0, 0, 0];
-    let mut image = File::create("tests/samples/beacon.gif").unwrap();
-    let mut encoder = Encoder::new(&mut image, WIDTH as u16, HEIGHT as u16, color_map).unwrap();
-    encoder.set_repeat(Repeat::Infinite).unwrap();
-    for state in states {
+            .fold(
+                [0; (WIDTH - MARGIN) * (HEIGHT - MARGIN) * 25],
+                |mut sum, curr| {
+                    for j in 0..5 {
+                        for i in 0..WIDTH - MARGIN {
+                            for k in 0..5 {
+                                sum[i + k + curr.0 * (WIDTH - MARGIN) * j] = curr.1[i];
+                            }
+                        }
+                    }
+                    sum
+                },
+            );
         let mut frame = Frame::default();
-        frame.width = WIDTH as u16;
-        frame.height = HEIGHT as u16;
-        frame.buffer = Cow::Borrowed(&state);
+        frame.width = (WIDTH - MARGIN) as u16;
+        frame.height = (HEIGHT - MARGIN) as u16;
+        frame.buffer = Cow::Borrowed(&curr);
         encoder.write_frame(&frame).unwrap();
+        state = state.update();
     }
 }
 
@@ -67,7 +81,7 @@ impl Life {
                     let (x, y) = (x as i32, y as i32);
                     for j in -1..=1 {
                         let new = (x + i, y + j);
-                        if new.0 < 0 || new.0 == WIDTH as i32 || new.1 < 0 || new.1 == HEIGHT as i32
+                        if new.0 < 0 || new.0 >= HEIGHT as i32 || new.1 < 0 || new.1 >= WIDTH as i32
                         {
                             continue;
                         }
